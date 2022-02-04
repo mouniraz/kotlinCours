3723fc0af0cc48b99cb8550bcbdb392b73863bbb
Zouaghi M <33163196+mouniraz@users.noreply.github.com>
2022-02-04 18:20:46 +0100

    Add files via upload

---
+++ Main.kt
@@ -0,0 +1,17 @@
+const val BEST_PROGRAMMING_LANGUAGE = "Kotlin"
+
+fun main() {
+
+    var hello = "Hello"
+    hello = "Hello world!"
+
+    println(hello)
+
+    var toto:String = "Toto"
+
+    println(toto)
+
+    var message: String? = "I’m learning Kotlin!"
+    println(message!!.uppercase())
+
+}
