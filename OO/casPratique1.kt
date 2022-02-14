71c1dcc709fd341ca89eb9452bafa395213cc806
Zouaghi M <33163196+mouniraz@users.noreply.github.com>
2022-02-14 16:11:42 +0100

    Delete casPratique1.kt

--- OO/casPratique1.kt
+++
@@ -1,9 +0,0 @@
-class Contact(var name:String ,var surname:String ,var addressLine1:String ,var addressLine2:String ,var email:String ,var mobileTel:String ,var homeTel:String ,var photo:String )
-fun main(args: Array<String>) {
-
-    
-    val contact=Contact("ahmed","toto","tunis","rrrrrr","34555","999999","44444","http://")
-    println(" name:${contact.name}  \n surname:${contact.surname} \n addressLine1:${contact.addressLine1} \n addressLine2:${contact.addressLine2} " +
-            "\n email:${contact.email} \n mobileTel:${contact.mobileTel} \n homeTel:${contact.homeTel}\n photo :${contact.homeTel}")
-    contact.mobileTel="5555555"
-}
